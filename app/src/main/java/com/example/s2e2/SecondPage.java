b2cde01656acc8429cb6fdd51d77cee7deddddb3
KimKiHyun0206 <rlarlgus0206@naver.com>
2022-10-16 18:09:47 +0900

    디자인 수정

--- app/src/main/java/com/example/s2e2/SecondPage.java
+++ app/src/main/java/com/example/s2e2/SecondPage.java
@@ -23,23 +23,4 @@ public class SecondPage extends Activity {
         Intent intent = new Intent(this, MapActivity.class);
         startActivity(intent);
     }
-
-    public void popUp(View view) {
-        Intent intent = new Intent(this, SecondPage.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "123")
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle("My notification")
-                .setContentText("Hello World!")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // Set the intent that will fire when the user taps the notification
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
-
-        notificationManagerCompat.notify(builder, builder.build());
-    }
 }
