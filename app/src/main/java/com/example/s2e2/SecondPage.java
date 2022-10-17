d79f12eeab6a1dd0bba889d4e389a292453363ff
KimKiHyun0206 <rlarlgus0206@naver.com>
2022-10-17 09:44:50 +0900

    배경화면 추가

--- app/src/main/java/com/example/s2e2/SecondPage.java
+++ app/src/main/java/com/example/s2e2/SecondPage.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import android.view.View;
@@ -17,6 +19,7 @@ public class SecondPage extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_sub);
+
     }
 
     public void onClick(View view) {
