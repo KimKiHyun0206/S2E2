c46bc2a99a42e13d2de79e2198d7d7144ef4b18a
KimKiHyun0206 <rlarlgus0206@naver.com>
2022-10-27 12:28:17 +0900

    색상 변경 및 알림 추가

--- app/src/main/java/com/example/s2e2/Alarm.java
+++ app/src/main/java/com/example/s2e2/Alarm.java
@@ -1,37 +1,20 @@
 package com.example.s2e2;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
-import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+public class Alarm extends Activity {
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //Intent intent2 = new Intent(this, SecondActivity.class);
-        //startActivity(intent2);
-
-    }
 
     public void clickBtn(View view) {
 
@@ -39,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         NotificationManager notificationManager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         //Notification 객체를 생성해주는 건축가객체 생성(AlertDialog 와 비슷)
-        android.support.v4.app.NotificationCompat.Builder builder= null;
+        NotificationCompat.Builder builder= null;
 
         //Oreo 버전(API26 버전)이상에서는 알림시에 NotificationChannel 이라는 개념이 필수 구성요소가 됨.
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
@@ -54,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             notificationManager.createNotificationChannel(channel);
 
             //알림건축가 객체 생성
-            builder=new android.support.v4.app.NotificationCompat.Builder(this, channelID);
+            builder=new NotificationCompat.Builder(this, channelID);
 
 
         }else{
@@ -84,26 +67,4 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-    private void getHashKey(){
-        PackageInfo packageInfo = null;
-        try {
-            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageInfo == null)
-            Log.e("KeyHash", "KeyHash:null");
-
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            } catch (NoSuchAlgorithmException e) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
-            }
-        }
-    }
-
 }
--- app/src/main/java/com/example/s2e2/MapActivity.java
+++ app/src/main/java/com/example/s2e2/MapActivity.java
@@ -1,28 +1,37 @@
 package com.example.s2e2;
 
 import android.app.Activity;
+
+import android.app.NotificationManager;
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
-import net.daum.mf.map.api.MapView;
+import androidx.core.app.NotificationCompat;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.core.app.NotificationManagerCompat;
 
-public class MapActivity extends Activity {
+public class MapActivity  extends Activity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
-        MapView mapView = new MapView(this);
-
-        ViewGroup mapViewContainer = findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
     }
 
-    public void onClick(View view){
+    public void onClick(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
     }
+
 }
